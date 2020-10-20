039c1ab5701de033cea4ed3747a59e393ae52673
Alberto Campos de la Torre <alberto.campos3077@alumnos.udg.mx>
2020-10-20 00:57:51 -0500

    Prueba 1

--- algoritmos.py
+++ algoritmos.py
@@ -1,16 +1,6 @@
-def distancia_euclidiana(x_1, y_1, x_2, y_2):
-    """ Calcula la distancia euclidiana
+import math
 
-    Devuelve el resultado de la formula
+def distanciaeuclidiana(x_1,y_1,x_2,y_2):
+    return math.sqrt((((x_2-x_1)**2)+((y_2-y_1)**2)))
+   
 
-    También se le conoe a la fórmula como:
-    distancia entre dos puntos
-
-    Parámetros:
-    x_1 -- origen_x
-    y_1 -- origen_y
-    x_2 -- destino_x
-    y_2 -- destino_y
-
-    """
-    
